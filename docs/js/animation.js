d364f4543e21e4661a05bdd110bfac46be185a43
Cesc Fàbregas <info@cescfabregasestudi.com>
2023-07-24 13:03:56 +0200

    Correció animacio menu i text cat i esp de John Willson

--- docs/js/animation.js
+++ docs/js/animation.js
@@ -3,7 +3,7 @@ var tl_homeHero = gsap.timeline({ defaults: { autoAlpha: 0, duration: 0.5 } });
 function init() {
 	tl_homeHero
 		.from(".home-hero", {})
-		.from(".home .site-header", { y: -48 })
+		.from(".site-header", { y: -48 })
 		.from(".home-hero__text", { y: 48 })
 		.from(".home-hero__img", { y: 24 })
 		.from(".home-hero__banner", { y: 80 });
