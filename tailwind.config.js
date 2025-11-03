8d2e96ba7ca4fb11570cb61302bc3fb451b47152
Elvia Benedith <116598037+ElviaBth@users.noreply.github.com>
2025-11-03 11:16:21 +0100

    Add blog page (#9)
    * add blog to config file
    
    * add blogs to collection
    
    * add draf input
    
    * add blogs to navigation
    
    * add more blog config
    
    * add css classes
    
    * add tailwind classes
    
    * delete file
    
    * add card styles
    
    * add blog layout
    
    * add media queries to blog
    
    * remove image
    
    * fix blog images styles
    
    * delete unused folder
    
    * appliy review suggestions
    
    * refactor config
    
    * change blog Id
    
    * refactor naming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
+const { gray } = require('@tailwindcss/typography/src/styles')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './**/*.html'
+    './**/*.html',
+    './**/**/*.html'
   ],
   darkMode: 'media',
   theme: {
     extend: {
+      colors: {
+        transparent: "transparent",
+        success: "#28A745",
+        alert: "#ED1C24",
+        warning: "#FFB703",
+        black: "#020203",
+        white: "#FFFFFF",
+        yellowpoke: "#FFE223",
+        gray: {
+          DEFAULT: "#6B7280CC", // 80% opacity
+          2: "#3E4C5C",
+          3: "#E1E5EF",
+          4: "#242424",
+          5: "#F6F8FA",
+          6: "#D3D5D9"
+        },
+      },
       typography: {
         DEFAULT: {
           css: {
@@ -31,6 +50,17 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+      xs: ["13px", "16px"],
+      sm: ["14px", "18px"],
+      md: ["16px", "20px"],
+      lg: ["18px", "23px"],
+      xl: ["20px", "25px"],
+      "2xl": ["24px", "30px"],
+      "3xl": ["32px", "40px"],
+      "4xl": ["36px", "45px"],
+      "5xl": ["48px", "60px"]
       },
     },
   },
